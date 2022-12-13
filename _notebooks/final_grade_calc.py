7b3c26aaa245b7eb144e3a070f78245ded906c89
paravsalaniwal <paravsalaniwal@gmail.com>
2022-12-12 23:08:25 -0800

    no kizzy drizzy

--- _notebooks/final_grade_calc.py
+++ _notebooks/final_grade_calc.py
@@ -1,7 +1,5 @@
 def grade_needed(current_grade, final_grade_weight, desired_grade):
 
-# Enter all values as percentages
-
   current_grade /= 100
   final_grade_weight /= 100
   desired_grade /= 100
@@ -12,7 +10,7 @@ final_grade_weight = float(input("Enter the weight of the final exam as a percen
 desired_grade = float(input("Enter your desired overall grade as a percentage: "))
 
 grade_needed = grade_needed(current_grade, final_grade_weight, desired_grade)
-print((current_grade))
-print((final_grade_weight))
-print((desired_grade))
+print("Your current grade is: " + str(current_grade))
+print("The final exam weight is: " + str(final_grade_weight))
+print("Your desired grade is: " + str(desired_grade))
 print(f"You need to get a grade of {grade_needed * 100}% on the final exam.")
